16a0a59bdd02b3f97b70cb1dbb9f0b2a773ebad7
sakshi-vyas <sakshivyas216@gmail.com>
2020-08-30 15:48:01 +0530

    Updates

---
+++ precache-manifest.d84f0c8cfa63035e520c9844c8d7adc2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7229f557c407e2615783716cc63de7f",
+    "url": "/FinalProject-Bootcamp-/index.html"
+  },
+  {
+    "revision": "03ad0f167b29607c483b",
+    "url": "/FinalProject-Bootcamp-/static/css/2.9fe260e6.chunk.css"
+  },
+  {
+    "revision": "33344bba18cac86b0059",
+    "url": "/FinalProject-Bootcamp-/static/css/main.730e1994.chunk.css"
+  },
+  {
+    "revision": "03ad0f167b29607c483b",
+    "url": "/FinalProject-Bootcamp-/static/js/2.8eceddb9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/FinalProject-Bootcamp-/static/js/2.8eceddb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33344bba18cac86b0059",
+    "url": "/FinalProject-Bootcamp-/static/js/main.e7e8d240.chunk.js"
+  },
+  {
+    "revision": "b04f46e9b86b59310b5f",
+    "url": "/FinalProject-Bootcamp-/static/js/runtime-main.e752983d.js"
+  },
+  {
+    "revision": "6fbf817aec3b765bc7152ddac2a9031d",
+    "url": "/FinalProject-Bootcamp-/static/media/audioBook.6fbf817a.jpg"
+  },
+  {
+    "revision": "236001b708089fa5eec261fd541594b2",
+    "url": "/FinalProject-Bootcamp-/static/media/contact.236001b7.jpg"
+  },
+  {
+    "revision": "8cfc2f9592e3da8dce4c402d1012af35",
+    "url": "/FinalProject-Bootcamp-/static/media/ill1.8cfc2f95.jpg"
+  },
+  {
+    "revision": "45353cf94be8ed80742d4dfbddc0ccae",
+    "url": "/FinalProject-Bootcamp-/static/media/ill2.45353cf9.jpg"
+  },
+  {
+    "revision": "f29d5f0030dd0115b87b1733c2a03a43",
+    "url": "/FinalProject-Bootcamp-/static/media/ill3.f29d5f00.jpg"
+  },
+  {
+    "revision": "d0d26086d9fe20c4299aa86f29105114",
+    "url": "/FinalProject-Bootcamp-/static/media/trial.d0d26086.jpg"
+  },
+  {
+    "revision": "0fb8c6cffde346d5128c12998acaac28",
+    "url": "/FinalProject-Bootcamp-/static/media/two.0fb8c6cf.jpg"
+  }
+]);
